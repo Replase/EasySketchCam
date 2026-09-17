147f7405447a40c93a47977c9c3c4aa8b772d396
Alan Emiliano Ramirez Ayala <75821310+Replase@users.noreply.github.com>
2026-09-16 21:16:27 -0600

    Initial Commit

---
+++ EasySketchCam/EasySketchCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasySketchCamApp.swift
+//  EasySketchCam
+//
+//  Created by Alan Emiliano Ramirez Ayala on 16/09/26.
+//
+
+import SwiftUI
+
+@main
+struct EasySketchCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
